c8f0163a51d35d08abf21f66b152efbb81f40ea1
jay <jaysbeverage@gmail.com>
2018-02-23 15:14:54 -0800

    finished phase 3

--- LRU/lib/p03_hash_set.rb
+++ LRU/lib/p03_hash_set.rb
@@ -1,26 +1,35 @@
 require_relative 'p02_hashing'
 
 class HashSet
-  attr_reader :count
+  attr_reader :count, :store
 
-  def initialize(num_buckets = 8)
+  def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(key)
+  def insert(num)
+    resize! if count == num_buckets
+    unless include?(num)
+      self[num] << num
+      @count += 1
+    end
   end
 
-  def include?(key)
+  def remove(num)
+    self[num].delete(num)
   end
 
-  def remove(key)
+  def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    idx = num.hash % num_buckets
+    @store[idx]
   end
 
   def num_buckets
@@ -28,5 +37,47 @@ class HashSet
   end
 
   def resize!
+
+    #reset whole store
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    @count = 0
+
+      @store.each do |el|
+        el.each do |x|
+          new_idx = x % (num_buckets * 2)
+          new_store[new_idx] << x
+          @count += 1
+        end
+      end
+
+      @store = new_store
   end
+  # attr_reader :count
+  #
+  # def initialize(num_buckets = 8)
+  #   @store = Array.new(num_buckets) { Array.new }
+  #   @count = 0
+  # end
+  #
+  # def insert(key)
+  # end
+  #
+  # def include?(key)
+  # end
+  #
+  # def remove(key)
+  # end
+  #
+  # private
+  #
+  # def [](num)
+  #   # optional but useful; return the bucket corresponding to `num`
+  # end
+  #
+  # def num_buckets
+  #   @store.length
+  # end
+  #
+  # def resize!
+  # end
 end
